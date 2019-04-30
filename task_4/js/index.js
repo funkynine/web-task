3c6384b51b1cc08ce426460b364ee0b4ef976ec4
Igor Bazyliuk <igorbazyliuk@hack.local>
2019-04-30 17:48:19 +0300

    Completed task-4

---
+++ task_4/js/index.js
@@ -0,0 +1,7 @@
+// забрати данні з інпутів та вивести в консоль по кліку на кнопку
+
+
+printData = () => {
+  console.log(document.getElementById('first').value);
+  console.log(document.getElementById('second').value);
+};
