314cbca4bb28f3cf32d920d539f1e544e7173499
Igor Bazyliuk <igorbazyliuk@hack.local>
2019-05-05 13:52:21 +0300

    Completed

---
+++ task_6/js/index.js
@@ -0,0 +1,79 @@
+// створити функцію яка буде переводити код morse в "людську" мову
+
+
+const MORSE_LIST = {
+
+    ".-":"A",
+    "-...":"B",
+    "-.-.":"C",
+    "-..":"D",
+    ".":"E",
+    "..-.":"F",
+    "--.":"G",
+    "....":"H",
+    "..":"I",
+    ".---":"J",
+    "-.-":"K",
+    ".-..":"L",
+    "--":"M",
+    "-.":"N",
+    "---":"O",
+    ".--.":"P",
+    "--.-":"Q",
+    ".-.":"R",
+    "...":"S",
+    "-":"T",
+    "..-":"U",
+    "...-":"V",
+    ".--":"W",
+    "-..-":"X",
+    "-.--":"Y",
+    "--..":"Z",
+    "/":" ",
+
+    "-----":"0",
+    ".----":"1",
+    "..---":"2",
+    "...--":"3",
+    "....-":"4",
+    ".....":"5",
+    "-....":"6",
+    "--...":"7",
+    "---..":"8",
+    "----.":"9",
+
+    "...---...": "SOS",
+
+    "-.-.--":"!",
+    ".-.-.-":".",
+    "--..--":","
+
+}
+
+// var re = new RegExp("");
+
+function morseDecoder(morseMessage) {
+
+    let words = morseMessage.trim().split("   ");
+    let decodedMessage = "";
+
+    for (let word of words) {
+        word = word.trim().split(" ");
+        for (let letter of word) {
+            letter = letter.trim();
+            decodedMessage += MORSE_LIST[letter];
+        }
+        decodedMessage += " ";
+    }
+    decodedMessage = decodedMessage.replace("undefined".trim(), "").trim();
+
+    return decodedMessage;
+}
+
+try{
+    console.log(morseDecoder('.... . .-.. .-.. ---      .. --. --- .-.'));
+}
+catch (error){
+    console.log(error);
+}
+
